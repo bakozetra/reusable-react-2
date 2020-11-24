d3429292578c4379d4bf53abe30cdfa21652813f
synthia <synthia.han@onja.org>
2020-11-24 16:31:11 +0300

    this is the firs

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const CharacterCounterInput = ({ text, defaults }) => {
+const CharacterCounterInput = ({ text, defaults , ...props }) => {
+	const [count , setCount] = useState("");
+	const [ textLength , setTextLength] = useState('');
+	const [max , setMax] = useState(0);
 	const maxLength = 280;
 
+	function addText (badm) {
+		setCount(prev => prev += badm)
+	}
+
 	return (
-		<div className="counterInput">
+		<div className={`counterInput ${count.length > maxLength && 'tooLong'}`}>
 			<div>
 				{defaults.map(b => {
-					return <button key={b}>{b}</button>;
+					return <button key={b} onClick={() => setCount(b)}>{b}</button>;
 				})}
 			</div>
-			<textarea placeholder={text} />
-			<div>0/{maxLength}</div>
+			<textarea placeholder={text}
+			 type ="text"
+			 value={count}
+			 onChange ={ (e) =>  {
+				setCount(e.currentTarget.value);
+			 }
+		}
+			/>
+			<div>{count.length}/{maxLength}</div>
 		</div>
 	);
 };
 
 const App = () => {
 	let defaultMoods = ['Great', 'Okay', 'Bad'];
-
 	return (
 		<section>
 			<h2>Mood Tracker</h2>
